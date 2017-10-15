07ae3824a97a766e0843c89eec0bb2ef07fbde95
Michael Nehas <Michael Nehas>
2017-10-15 19:08:20 -0400

    friendfinder

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendMatch = [
+	{
+	  "name":"Tom Hardy",
+	  "photo":"https://vignette.wikia.nocookie.net/dcmovies/images/4/45/Tom_Hardy.jpg/revision/latest?cb=20141203093341.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	},
+	{
+	  "name":"Sofia Vergara",
+	  "photo":"http://iwatchstuff.com/2011/04/07/sofia-vergara.jpg",
+	  "scores":[
+	     1,
+	     2,
+	     5,
+	     5,
+	     1,
+	     2,
+	     3,
+	     1,
+	     5,
+	     2
+	   ]
+	},
+	{
+	  "name":"Jason Momoa",
+	  "photo":"https://vignette.wikia.nocookie.net/dcmovies/images/8/8d/Jason_Momoa.jpg/revision/latest?cb=20140702001326.jpg",
+	  "scores":[
+	     4,
+	     5,
+	     3,
+	     3,
+	     4,
+	     5,
+	     1,
+	     4,
+	     3,
+	     5
+	   ]
+	},
+	{
+	  "name":"Blake Lively",
+	  "photo":"https://i.pinimg.com/originals/db/85/c7/db85c7167ae63601e6862599fd16528a.jpg",
+	  "scores":[
+	     2,
+	     3,
+	     1,
+	     1,
+	     2,
+	     3,
+	     4,
+	     2,
+	     1,
+	     3
+	   ]
+	}
+];
+
+module.exports = friendMatch;
