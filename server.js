8d3498b48e7e7684c2356bfe49a5c75099ed1ad4
Michael Nehas <Michael Nehas>
2017-10-16 21:49:41 -0400

    friendifnder

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-require('/app/routing/apiRoutes.js')(app); 
-require('/app/routing/htmlRoutes.js')(app);
 
 
 
